ae7aac828e05a0df6144a976773a3844be3ce876
ihriyad <ihriyad.dev@gmail.com>
2026-06-24 18:54:54 +0600

    added file structure and ico with first commit

---
+++ src/app/(public)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PublicPageLayout = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default PublicPageLayout;
---
+++ src/app/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const GlobalErrorPage = () => {
+    return (
+        <div>
+            An error occurred.
+        </div>
+    );
+};
+
+export default GlobalErrorPage;
